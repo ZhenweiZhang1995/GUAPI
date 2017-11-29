048fcbb469e83190ad37de71cfc9cdead71aa4e0
Zhenwei Zhang <zhenwei@live.unc.edu>
2017-11-28 21:12:54 -0500

    spinner

--- app/src/main/java/guapi/guapi/MainActivity.java
+++ app/src/main/java/guapi/guapi/MainActivity.java
@@ -6,8 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.SupportMapFragment;
 
@@ -19,15 +23,24 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton btn_gps;
     private ImageButton btn_tour;
+<<<<<<< Updated upstream
     SQLiteDatabase db = null;
+=======
+    private Spinner spinner1;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner1.setOnItemSelectedListener(new ItemSelectedListener());
+
         btn_gps = findViewById(R.id.gps);
         btn_tour = findViewById(R.id.tour);
+
+
 
         btn_gps.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+<<<<<<< Updated upstream
     public void resetDB(){
         db.execSQL("DROP TABLE IF EXISTS Landmarks");
         db.execSQL("CREATE TABLE IF NOT EXISTS Landmarks (ID TEXT,Name TEXT, Location TEXT, Description TEXT)");
@@ -61,4 +75,28 @@ public class MainActivity extends AppCompatActivity {
         db.execSQL("INSERT INTO Landmarks VALUES('ow','Old Well','35.912360, -79.051219','At the heart of campus stands the visual symbol of the University of North Carolina at Chapel Hill. For many years the Old Well served as the sole water supply for Old East and Old West dormitories.')");
         db.execSQL("INSERT INTO Landmarks VALUES('pt','Playmakers Theater','35.916313, -79.053548','The most beautiful building on the Carolina campus, to many tastes, is this Greek Revival temple considered to be one of the masterworks of New York architect Alexander Jackson Davis. He designed the building as an unlikely combination library and ballroom; later it was used for agricultural chemistry and law. For many years, it was the theatre of the Carolina Playmakers, who were largely responsible for developing folk drama in the United States. Instead of the acanthus leaves that usually ornament Corinthian capitals, Davis substituted wheat and Indian corn, in response to the aggressive Americanism then present in the country.')");
            }
+=======
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        //get strings of first item
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+                // ToDo when first item is selected
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+                // Todo when item is selected by the user
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+
+    }
+>>>>>>> Stashed changes
 }
